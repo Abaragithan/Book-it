4efcc84f37a1e43717bf46d4ef875e328d909796
abaragithan <abaragithan02@gmail.com>
2026-03-01 17:41:22 +0530

    single room view page added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Heading title="Available Rooms" />
       {
         rooms.length > 0 ? (
           rooms.map((room) => (
@@ -13,6 +15,6 @@ export default function Home() {
           <p className="text-center py-20 text-gray-600">No rooms available.</p>
         )
       }
-    </div>
+    </>
   );
 }
--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -3,38 +3,38 @@ import Link from "next/link"
 
 const RoomCard = ({ room }) => {
     return (
-        <div
-            class="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center"
+        <div key={room.$id}
+            className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center"
         >
-            <div class="flex flex-col sm:flex-row sm:space-x-4">
+            <div className="flex flex-col sm:flex-row sm:space-x-4">
                 <Image
                     src={`/images/rooms/${room.image}`}
                     alt={room.name}
                     width={400}
                     height={100}
-                    class="w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg"
+                    className="w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg"
                 />
-                <div class="space-y-1">
-                    <h4 class="text-lg font-semibold">{room.name}</h4>
-                    <p class="text-sm text-gray-600">
-                        <span class="font-semibold text-gray-800"> Address:</span> {room.address}
+                <div className="space-y-1">
+                    <h4 className="text-lg font-semibold">{room.name}</h4>
+                    <p className="text-sm text-gray-600">
+                        <span className="font-semibold text-gray-800"> Address:</span> {room.address}
                     </p>
-                    <p class="text-sm text-gray-600">
-                        <span class="font-semibold text-gray-800"> Availability:</span>
+                    <p className="text-sm text-gray-600">
+                        <span className="font-semibold text-gray-800"> Availability:</span>
                         {room.availability}
                     </p>
-                    <p class="text-sm text-gray-600">
-                        <span class="font-semibold text-gray-800"> Price:</span>
+                    <p className="text-sm text-gray-600">
+                        <span className="font-semibold text-gray-800"> Price:</span>
                         ${room.price}/hour
                     </p>
                 </div>
             </div>
             <div
-                class="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
+                className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
             >
                 <Link
                     href={`/rooms/${room.$id}`}
-                    class="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
+                    className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
                 >View Room</Link
                 >
             </div>
